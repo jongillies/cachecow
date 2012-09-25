e7adf058fef209fa7f235b14eb2c4d64ee6515a4
Jon Gillies <jon@gillies.us>
2012-09-24 19:21:22 -0700

    Added link_to_if_not_zero method
    Updated views not to link of the total of something is zero.

--- www/app/helpers/application_helper.rb
+++ www/app/helpers/application_helper.rb
@@ -1,6 +1,7 @@
 module ApplicationHelper
 
-  def link_if_not_zero number, link
+  def link_to_if_not_zero number, link
+    raise "Number does not respond to to_i :-(" unless number.respond_to? :to_i
     if number == 0
       number
     else
