f49b87f29104c460ea592b010ef9f16961f04e39
Jon Gillies <jon@gillies.us>
2012-09-24 19:04:02 -0700

    Refacted views
    created helper to only link_to if the 1st parameter is not zero.
    
    Added a test for subscribers model.

--- www/app/helpers/application_helper.rb
+++ www/app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+
+  def link_if_not_zero number, link
+    if number == 0
+      number
+    else
+      link_to number, link
+    end
+  end
 end
--- www/test/unit/subscriber_test.rb
+++ www/test/unit/subscriber_test.rb
@@ -5,4 +5,9 @@ class SubscriberTest < ActiveSupport::TestCase
     assert Subscriber.all.count == 2
   end
 
+  test "subscriber 1 has 2 subscriptions" do
+    assert Subscriber.find(1).subscriptions.count == 2
+  end
+
+
 end
