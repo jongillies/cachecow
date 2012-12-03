b9a44c290d859d6a759b9003c26962d7cf2ce9bd
Jon Gillies <jon@gillies.us>
2012-12-03 16:46:47 -0700

    Some updates

--- www/app/helpers/application_helper.rb
+++ www/app/helpers/application_helper.rb
@@ -1,6 +1,5 @@
 module ApplicationHelper
 
-
   def link_to_if_not_zero number, link
     raise "Number does not respond to to_i :-(" unless number.respond_to? :to_i
     if number == 0
@@ -10,4 +9,14 @@ module ApplicationHelper
     end
   end
 
+  def my_json_diff(a,b)
+
+      a_hash = Crack::JSON.parse(a)
+      b_hash = Crack::JSON.parse(b)
+      c = a_hash.diff(b_hash)
+      #
+      diff_js = JSON.pretty_generate(c)
+      #
+      Pygments.highlight(diff_js, :formatter => 'text', :lexer => 'javascript')
+  end
 end
